ee103861e920da5457d6f70dc9e29a334ed544c7
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-01-27 20:34:52 +0100

    Remove "APP_DIRS" in TEMPLATES

--- src/django_htmx_todo/settings.py
+++ src/django_htmx_todo/settings.py
@@ -104,7 +104,6 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [package_resource("templates")],
-        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
