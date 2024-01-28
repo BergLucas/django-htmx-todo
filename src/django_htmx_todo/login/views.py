76141932ac3d43d7552742d8a59328bb8e93f663
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-01-28 22:20:49 +0100

    Add auto redirect when already logged in

--- src/django_htmx_todo/login/views.py
+++ src/django_htmx_todo/login/views.py
@@ -1,10 +1,17 @@
+from typing import Any
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
-
 
 class LoginView(DjangoLoginView):
     template_name = "login.html"
     next_page = reverse_lazy("list_tasks")
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(self.next_page)
+        else:
+            return super().get(request, *args, **kwargs)
 
 class LogoutView(DjangoLogoutView):
     next_page = reverse_lazy("login")
