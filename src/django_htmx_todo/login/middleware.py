a898f6148071237cb6f80623769d703dc30ce2c6
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-01-28 22:59:07 +0100

    Improve htmx_middleware

--- src/django_htmx_todo/login/middleware.py
+++ src/django_htmx_todo/login/middleware.py
@@ -1,23 +1,13 @@
-from urllib.parse import urlparse
+from django_htmx.http import HttpResponseClientRedirect
+from django.http import HttpRequest, HttpResponse
 
 
 def htmx_middleware(get_response):
-    def middleware(request):
-        response = get_response(request)
-        if (
-            request.headers.get("HX-Request") == "true"
-            and response.status_code == 302
-        ):
-            ref_header = request.headers.get("Referer", "")
-            if ref_header:
-                referer = urlparse(ref_header)
-                querystring = f"?next={referer.path}"
-            else:
-                querystring = ""
-
-            redirect = urlparse(response["location"])
-            response.status_code = 204
-            response.headers["HX-Redirect"] = f"{redirect.path}{querystring}"
-        return response
+    def middleware(request: HttpRequest) -> HttpResponse:
+        response: HttpResponse = get_response(request)
+        if (request.headers.get("HX-Request") == "true" and response.status_code == 302):
+            return HttpResponseClientRedirect(response["location"])
+        else:
+            return response
 
     return middleware
