7e55b5bee364662f53e4ceaf9fb1f60d19b5732c
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-01-28 17:38:24 +0100

    Add delete view and fix update view

--- src/django_htmx_todo/todo/urls.py
+++ src/django_htmx_todo/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django_htmx_todo.todo.views import ListTaskView, UpdateTaskView, TaskPreview
+from django_htmx_todo.todo.views import ListTaskView, UpdateTaskView, DeleteTaskView, UpdateTaskCompletedView
 
 urlpatterns = [
-    path("", ListTaskView.as_view(), name="tasks_view"),
-    path("<int:pk>/completed/", TaskPreview.as_view(), name="update_task_completed"),
+    path("", ListTaskView.as_view(), name="list_tasks"),
+    path("<int:pk>/", DeleteTaskView.as_view(), name="delete_task"),
     path("<int:pk>/update/", UpdateTaskView.as_view(), name="update_task"),
+    path("<int:pk>/update/completed/", UpdateTaskCompletedView.as_view(), name="update_task_completed"),
 ]
--- src/django_htmx_todo/todo/views.py
+++ src/django_htmx_todo/todo/views.py
@@ -1,11 +1,11 @@
-from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, UpdateView
-from django.shortcuts import get_object_or_404
-from django_htmx_todo.todo.components.task_preview.task_preview import TaskPreview
+from django.http import HttpRequest
+from django.views.generic import View, TemplateView, UpdateView
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy
+from django_htmx.http import HttpResponseLocation
 from django_htmx_todo.todo.models import Task
 from django_htmx_todo.todo.forms import TaskForm
-from django_htmx.http import HttpResponseClientRedirect
 
 
 class ListTaskView(LoginRequiredMixin, TemplateView):
@@ -16,8 +16,30 @@ class ListTaskView(LoginRequiredMixin, TemplateView):
         return {"tasks": tasks}
 
 
+class DeleteTaskView(LoginRequiredMixin, View):
+    model = Task
+    success_url = reverse_lazy("list_tasks")
+
+    def delete(self, request: HttpRequest, pk: int) -> HttpResponseLocation:
+        task = get_object_or_404(self.model, pk=pk)
+        task.delete()
+        return HttpResponseLocation(self.success_url)
+
+
+class UpdateTaskCompletedView(LoginRequiredMixin, View):
+    template_name = "task_preview.html"
+    model = Task
+    success_url = reverse_lazy("list_tasks")
+
+    def patch(self, request: HttpRequest, pk: int) -> HttpResponseLocation:
+        task = get_object_or_404(Task, pk=pk)
+        task.completed = not task.completed
+        task.save()
+        return render(request, self.template_name, {"task": task})
+
+
 class UpdateTaskView(LoginRequiredMixin, UpdateView):
     template_name = "update_task.html"
     form_class = TaskForm
     model = Task
-    success_url = "/tasks/"
+    success_url = reverse_lazy("list_tasks")
